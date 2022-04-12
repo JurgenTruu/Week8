aa78fed99aafd58ab7adf54906834adf12a6b792
Jürgen Truu <jtruu@tktk.ee>
2022-04-12 16:04:21 +0300

    Control Flow

---
+++ PickFromFile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace PickFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomFood, randomDrink, randomMovie;
+            randomFood = getRandomFromFile("foods.txt");
+            randomDrink = getRandomFromFile("drinks.txt");
+            randomMovie = getRandomFromFile("movies.txt");
+            Console.WriteLine($"Tonight you will have some {randomFood} with {randomDrink} and watch {randomMovie}");
+            
+            
+
+        }
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            return randomIndex;
+        }
+        private static string getRandomFromFile(string fileName)
+        {
+            string filePath = $@"C:\Users\...\samples\{fileName}";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            string randomElement = dataFromFile[GenerateRandomIndex(dataFromFile)];
+            return randomElement;
+        }
+    }
+}
